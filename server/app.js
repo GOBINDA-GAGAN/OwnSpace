d718932ddc2b533f40ebfa15e5c98d0cb326dc3e
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-08-28 12:44:56 +0530

    backend learn

--- server/app.js
+++ server/app.js
@@ -1,20 +1,59 @@
+import { log } from "console";
 import express from "express";
-import { readdir } from "fs/promises";
+import { readdir, rm } from "fs/promises";
 const app = express();
 console.log(app);
+app.use(express.json())
 
 const port = 4000;
 app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*");
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers": "*",
+  });
   next();
 });
-app.use((req, res, next) => {
-  if(req.query.action==="download"){
-    res.set("content-Disposition","attachment")
+
+app.get("/:filename", (req, res) => {
+  try {
+    const { filename } = req.params;
+    if (req.params.action === "download") {
+      res.set("Content-Disposition", "attachment");
+    }
+
+    res.sendFile(`${import.meta.dirname}/storage/${filename}`);
+  } catch (error) {
+    console.log(error.message);
   }
-  
-  const serveFile = express.static("storage");
-  serveFile(req, res, next);
+});
+app.delete("/:filename", async(req, res) => {
+  try {
+    const { filename } = req.params;
+    console.log(filename);
+    const filePath = `${import.meta.dirname}/storage/${filename}`;
+    try {
+      await rm(filePath);
+      res.json({ message: "file deleted" });
+    } catch (error) {
+      console.log(error.message);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.patch("/:filename", async(req, res) => {
+  try {
+    const { filename } = req.params;
+    const {newFilename}=req.body;
+    console.log(newFilename);
+    
+    console.log(filename);
+    
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.get("/", async (req, res) => {
